bd0cd897d9a2aad2c24064217f7e98f76e82d0ac
Sandra Tatarevićová <sandra@tatarevicova.cz>
2014-10-16 22:15:23 +0200

    refactoring: replaced MAX_DIST constant with INT_MAX

--- src/main.c
+++ src/main.c
@@ -7,12 +7,12 @@
 #include <stdio.h>
 #include <tgmath.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "char_operations.h"
 #include "nsr_string.h"
 #include "nsr_io.h"
 
 #define TESTING_RESULT_SIZE 3
-#define MAX_DIST 999
 /*
 int main(int argc, char *argv[]) {
     int result;
@@ -49,13 +49,13 @@ int main(int argc, char **argv)
    input = fopen(argv[1], "r");
 
    nsr_read_strings(input, strings);
-   result->_count = strings->_count;
-   result->_total_distance = MAX_DIST;
-   result->_distances = (int*) malloc(result->_count*sizeof(int));
+   result->_distances_count = strings->_count;
+   result->_total_distance = INT_MAX;
+   result->_distances = (int*) malloc(result->_distances_count*sizeof(int));
    result->_string = (char*) malloc(TESTING_RESULT_SIZE*sizeof(char)+1);
    nsr_strings_print(strings);
 
-   all_words_rec(compareString,strings,TESTING_RESULT_SIZE,0,MAX_DIST,result);
+   all_words_rec(compareString,strings,TESTING_RESULT_SIZE,0,INT_MAX,result);
    printf("Result string is \'%s\' with total distance %d.\n",result->_string,
            result->_total_distance);
    for(i = 0; i < strings->_count; i++)
