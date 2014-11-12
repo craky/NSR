abab1a084a1b64091f891a3a3a265d5b6d418e0f
Vojtech Krakora <v.krakora@gmail.com>
2014-11-12 20:34:46 +0100

    Added function for send MSG_FINIS to all processes except the process 0.

--- src/proc_com.c
+++ src/proc_com.c
@@ -1,5 +1,20 @@
 #include "proc_com.h"
 
-int acz_ahd(const int loc_counter, const int proc_sum){    
+#include <mpi.h>
+
+int acz_ahd(const int loc_counter, const int proc_sum)
+{    
     return (loc_counter+1)%proc_sum;
+}
+
+void finish_processes(void)
+{
+    int i = 0, proc_num = 0;
+    
+     /* find out number of processes */
+     MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+     
+     for( i = 1; i < proc_num; i++)
+      MPI_Send(NULL,0,MPI_CHAR,i,MSG_FINISH,
+              MPI_COMM_WORLD);
 }
--- src/proc_com.h
+++ src/proc_com.h
@@ -2,6 +2,8 @@
 #ifndef __PROC_COMM_H__
 #define __PROC_COMM_H__
 
+#define MSG_FINISH       1004
+
 /**
  * 
  * Algorithms for donor search
@@ -13,5 +15,11 @@
  * @return Next donor id
  */
 int acz_ahd(const int loc_counter, const int proc_sum);
+
+/**
+ * Will finish all processes except the process 0
+ * Needs to be after MPI_Init() and before MPI_Finalize()
+ */
+void finish_processes(void);
 
 #endif
