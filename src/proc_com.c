be0bacfedcecda067ca5169e8e9955ea9832898a
Vojtech Krakora <v.krakora@gmail.com>
2014-11-12 23:00:06 +0100

    Refactoring. Added Function for sending a work.

--- src/proc_com.c
+++ src/proc_com.c
@@ -1,13 +1,16 @@
 #include "proc_com.h"
 
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int acz_ahd(const int loc_counter, const int proc_sum)
 {    
     return (loc_counter+1)%proc_sum;
 }
 
-void finish_processes(void)
+void proc_com_finish_processes(void)
 {
     int i = 0, proc_num = 0;
     
@@ -17,4 +20,18 @@ void finish_processes(void)
      for( i = 1; i < proc_num; i++)
       MPI_Send(NULL,0,MPI_CHAR,i,MSG_FINISH,
               MPI_COMM_WORLD);
+}
+
+void proc_com_send_work(nsr_stack_t *stack, const int dest_proc, const int str_size)
+{
+    char *buffer = (char*) malloc((str_size+1)*sizeof(char));
+    
+    nsr_stack_elem_t elem = nsr_stack_pop_bottom(stack);
+    memcpy(buffer,elem._string,str_size);
+    buffer[str_size] = elem._idx;
+    
+    /* create message */
+    MPI_Send(buffer,str_size+1,MPI_CHAR,dest_proc,MSG_WORK_SENT,MPI_COMM_WORLD);
+    
+    free(buffer);
 }
--- src/proc_com.h
+++ src/proc_com.h
@@ -2,7 +2,11 @@
 #ifndef __PROC_COMM_H__
 #define __PROC_COMM_H__
 
+#include "nsr_stack.h"
+
+#define MSG_WORK_SENT    1001
 #define MSG_FINISH       1004
+#define BUFFER_SIZE      100
 
 /**
  * 
@@ -20,6 +24,8 @@ int acz_ahd(const int loc_counter, const int proc_sum);
  * Will finish all processes except the process 0
  * Needs to be after MPI_Init() and before MPI_Finalize()
  */
-void finish_processes(void);
+void proc_com_finish_processes(void);
+
+void proc_com_send_work(nsr_stack_t *stack, const int dest_proc, const int str_size);
 
 #endif
